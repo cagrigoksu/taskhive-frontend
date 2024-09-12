52435d2c3d0033eab75b4755b2e821ccdeab8063
Cagri Goksu <cagrigoksu.ustundag@gmail.com>
2024-09-12 23:09:26 +0200

    Header menu design fix.

--- taskhive/src/app/parts/taskhive-header/taskhive-header.component.ts
+++ taskhive/src/app/parts/taskhive-header/taskhive-header.component.ts
@@ -4,11 +4,12 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-taskhive-header',
   standalone: true,
-  imports: [NgIf, MatButtonModule],
+  imports: [NgIf, MatButtonModule, MatMenuModule],
   templateUrl: './taskhive-header.component.html',
   styleUrl: './taskhive-header.component.css'
 })
