c50c15e91ad4fc58815aedb4408f8c1e3e56901e
Cagri Goksu <cagrigoksu.ustundag@gmail.com>
2024-09-16 16:19:16 +0200

    Security settings done.

--- taskhive/src/app/pages/profile-settings/security-settings/security-settings.component.ts
+++ taskhive/src/app/pages/profile-settings/security-settings/security-settings.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { DataTransferService } from '../../../services/data-transfer.service';
+import { UserService } from '../../../services/user.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-security-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInput,MatFormField, MatLabel, NgIf, MatButton],
   templateUrl: './security-settings.component.html',
   styleUrl: './security-settings.component.css'
 })
 export class SecuritySettingsComponent {
+
+  dts = inject(DataTransferService);
+  UserService = inject(UserService);
+
+  protected userPasswordChangeForm = new FormGroup({
+    currentPassword: new FormControl(''),
+    newPassword: new FormControl(''),// [Validators.required]),
+    newPasswordConfirmation: new FormControl('')
+  })
+
+  uuserPasswordChangeData !: any;
+
+  changePassword() {
+
+  }
 
 }
